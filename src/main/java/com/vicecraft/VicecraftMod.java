6401dfad986769d03ecd767738c07766777c4553
Kruassa4ekUwU <kruassa4ek@gmail.com>
2026-07-07 12:50:28 +0300

    Implement VicecraftMod class with registration logic

---
+++ src/main/java/com/vicecraft/VicecraftMod.java
@@ -0,0 +1,27 @@
+package com.vicecraft;
+
+import com.vicecraft.item.ModItems;
+import com.vicecraft.block.ModBlocks;
+import com.vicecraft.effect.ModEffects;
+import net.neoforged.bus.api.IEventBus;
+import net.neoforged.fml.common.Mod;
+import net.neoforged.fml.ModContainer;
+import net.neoforged.neoforge.common.NeoForge;
+
+@Mod(VicecraftMod.MOD_ID)
+public class VicecraftMod {
+    public static final String MOD_ID = "vicecraft";
+
+    public VicecraftMod(IEventBus modEventBus, ModContainer modContainer) {
+        ModEffects.register(modEventBus);
+        ModItems.register(modEventBus);
+        ModBlocks.register(modEventBus);
+
+        modEventBus.addListener(this::commonSetup);
+        NeoForge.EVENT_BUS.register(this);
+    }
+
+    private void commonSetup(final net.neoforged.fml.event.lifecycle.FMLCommonSetupEvent event) {
+        // место для общей инициализации (например регистрация compat-хуков Create/Farmer's Delight)
+    }
+}
